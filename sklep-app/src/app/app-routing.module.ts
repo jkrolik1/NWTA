55050b52ce0ea02a4dbd86c78564a877ad951a8d
AJaszek <31315854+AJaszek@users.noreply.github.com>
2021-03-18 21:24:40 +0100

    Add files via upload

---
+++ sklep-app/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { ItemComponent } from './item/item.component';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { ShopComponent } from './shop/shop.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/shop', pathMatch: 'full' },
+  { path: 'order', component: OrderComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'item/:itemId', component: ItemComponent },
+  { path: 'shop', component: ShopComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
